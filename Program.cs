d2d9bb2c0a892b5f086a3d5b63becc520a6fd95e
Brian Trager <brian.p.trager@outlook.com>
2019-08-27 20:42:46 -0400

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   PracticeMakesPerfect.csproj 	new file:   Program.cs 	new file:   bin/Debug/netcoreapp2.2/PracticeMakesPerfect.deps.json 	new file:   bin/Debug/netcoreapp2.2/PracticeMakesPerfect.dll 	new file:   bin/Debug/netcoreapp2.2/PracticeMakesPerfect.pdb 	new file:   bin/Debug/netcoreapp2.2/PracticeMakesPerfect.runtimeconfig.dev.json 	new file:   bin/Debug/netcoreapp2.2/PracticeMakesPerfect.runtimeconfig.json 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.AssemblyInfo.cs 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.AssemblyInfoInputs.cache 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.assets.cache 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.csproj.FileListAbsolute.txt 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.csprojAssemblyReference.cache 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.dll 	new file:   obj/Debug/netcoreapp2.2/PracticeMakesPerfect.pdb 	new file:   obj/PracticeMakesPerfect.csproj.nuget.cache 	new file:   obj/PracticeMakesPerfect.csproj.nuget.dgspec.json 	new file:   obj/PracticeMakesPerfect.csproj.nuget.g.props 	new file:   obj/PracticeMakesPerfect.csproj.nuget.g.targets 	new file:   obj/project.assets.json

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PracticeMakesPerfect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("This is very cool!");
+            System.Console.WriteLine("GitHub rocks!!");
+        }
+    }
+}
